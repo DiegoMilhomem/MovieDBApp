6e8333a03021991948a4262a2257a059dcba408e
DIEGO DA SILVA MILHOMEM <diegosmilhomem23@hotmail.com>
2019-10-29 21:56:56 -0300

    Implementação dos Modelos de Classes e Implementação das Classes de Requisição HTTP utilizando tanto Newtonsoft como System.Text.Json

---
+++ MovieDBApp/MovieDBApp/MovieDBApp/Services/IMovieDBService.cs
@@ -0,0 +1,11 @@
+﻿using MovieDBApp.Models;
+using System.Threading.Tasks;
+
+namespace MovieDBApp.Services
+{
+    public interface IMovieDBService
+    {
+        Task<SearchResponse<Movie>> GetUpcomingMoviesAsync(int pageNumber = 1, string language = "en");
+        Task<Movie> FindByIdAsync(int movieId, string language = "en");
+    }
+}
